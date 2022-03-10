dc2a3815d8b52ef7a73a221bb5e5f81176fc3663
rshahriar98 <100007504+rshahriar98@users.noreply.github.com>
2022-03-10 16:01:05 -0500

    Create java_Variables.java

---
+++ java_Variables.java
@@ -0,0 +1,21 @@
+package Java_Basic;
+/*whenever creating a variable you always have to declare the data type of the variable */
+public class java_Variables {
+
+	int myNum = 15;
+	
+	public static void main(String[] args) {
+	
+		int myNum = 15;
+		// here i declared myNum as an int datatype
+		System.out.println(myNum);
+
+	}
+
+}
+
+
+
+
+Output:
+15
